85910049e624452a9a1546fd07c8c9032b9b8ad0
Muhammad Naeem <inaeem@protonmail.com>
2023-02-19 18:12:46 +0500

    Add quick docs to paperpk

--- jx/spiders/paperpk.py
+++ jx/spiders/paperpk.py
@@ -1,10 +1,15 @@
+"""paperpk.py - A scrapy spider to scrape https://paperpk.com"""
 import scrapy
 
 
-class PaperpkSpider(scrapy.Spider):
+class PaperPKSpider(scrapy.Spider):
+    """A scrapy spider that scrapes content from PaperPk"""
     name = "paperpk"
     allowed_domains = ["paperpk.com"]
-    start_urls = ["http://paperpk.com/"]
+
+    def start_requests(self):
+        url: str = ""
+        return scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         pass
