80dcfed661a6ddfddc3874a4eddac3ecff4d106d
Muhammad Naeem <inaeem@protonmail.com>
2023-02-19 19:09:33 +0500

    add css selector to scrape company name

--- jx/spiders/paperpk.py
+++ jx/spiders/paperpk.py
@@ -1,15 +1,22 @@
-"""paperpk.py - A scrapy spider to scrape https://paperpk.com"""
+from datetime import date
 import scrapy
 
 
 class PaperPKSpider(scrapy.Spider):
-    """A scrapy spider that scrapes content from PaperPk"""
+
     name = "paperpk"
     allowed_domains = ["paperpk.com"]
 
     def start_requests(self):
-        url: str = ""
-        return scrapy.Request(url=url, callback=self.parse)
+        today = date.today()
+        url = f"http://paperpk.com/calendar-search.php?req={today}"
+        yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        pass
+        rows = response.xpath('//div[@id="job-table"]//tr[count(.//td)=3]')
+
+        for row in rows:
+            yield {
+                "company_name": row.css('a#company_sub_link::text').get(),
+                "post_title": row.css('td a:first-of-type::text').get()
+            }
